db2da1f8d4fc09e9a528ba1edb3e5362f206733d
JesusHdezVL <jesus.hernandez@vlesting.onmicrosoft.com>
2025-01-15 12:29:38 -0600

    update in new endpoints, add enviroments variables

--- server.js
+++ server.js
@@ -2,67 +2,302 @@ const express = require('express');
 const sql = require('mssql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const env_var = require('dotenv').config();
+const { format } = require('date-fns');
+const { es } = require('date-fns/locale');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Configuración de conexión a Azure SQL
 const dbConfig = {
-    user: 'admin_viajes_lili',
-    password: '$3<Leca8#LP,5y9',
-    server: 'server-viajes-lili.database.windows.net',
-    database: 'db_viajes_lili',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
-        encrypt: true, // Asegura la conexión
+        encrypt: true,
     }
 };
 
-sql.connect(dbConfig).then(pool => {
-    console.log('Conexión exitosa a Azure SQL');
 
-    // Endpoint para obtener datos
-    app.get('/getData', async (req, res) => {
+let pool;
+
+// Función para conectar a la base de datos con reintentos
+const connectWithRetry = async () => {
+    try {
+        console.log('Intentando conectar a Azure SQL...');
+        pool = await sql.connect(dbConfig);
+        console.log('Conexión exitosa a Azure SQL');
+    } catch (err) {
+        console.error('Error al conectar a la base de datos:', err.message);
+        setTimeout(connectWithRetry, 5000); // Reintenta después de 5 segundos
+    }
+};
+
+// Llamar a la función de conexión
+connectWithRetry();
+
+// Middleware para verificar la conexión antes de cada request
+app.use(async (req, res, next) => {
+    if (!pool || !pool.connected) {
         try {
-            const result = await pool.request().query('SELECT * FROM conciliacion');
-            res.status(200).json(result.recordset);
+            console.log('Reconectando a la base de datos...');
+            await connectWithRetry();
         } catch (err) {
-            res.status(500).send(err.message);
+            console.error('No se pudo reconectar:', err.message);
+            return res.status(500).json({ message: 'Error al conectar a la base de datos' });
         }
-    });
+    }
+    next();
+});
 
-    app.post('/addDataLog/', async (req, res) => {
-        const { id, usr, asesor, asesor_ant, corsario,corsario_ant, recibo, recibo_ant, fecha_validacion, fecha_validacion_ant } = req.body; 
-        console.log(req.body);
-        if (!id && !usr && !asesor && !asesor_ant && !corsario && !corsario_ant && !recibo && !recibo_ant && !fecha_validacion && !fecha_validacion_ant) {
-            return res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar' });
+// Endpoints
+app.post('/getData', async (req, res) => {
+    const { filters } = req.body;
+
+    // Construir la consulta dinámica con filtros
+    let query = 'SELECT * FROM conciliacion WHERE 1=1'; // 1=1 permite concatenar condiciones fácilmente
+
+    if (filters.id) {
+        query += ' AND id = @id';
+    }
+
+    if (filters.fecha) {
+        query += ' AND fecha = @fecha';
+    }
+
+    if (filters.descripcion) {
+        query += ' AND descripcion LIKE @descripcion';
+    }
+
+    if (filters.referencia) {
+        query += ' AND referencia LIKE @referencia';
+    }
+
+    if (filters.referencia_ampliada) {
+        query += ' AND referencia_ampliada LIKE @referencia_ampliada';
+    }
+
+    if (filters.cargo) {
+        query += ' AND estatus = @cargo';
+    }
+
+    if (filters.proveedor) {
+        query += ' AND proveedor = @proveedor';
+    }
+
+    if (filters.asesor) {
+        query += ' AND asesor = @asesor';
+    }
+
+    if (filters.corsario) {
+        query += ' AND corsario = @corsario';
+    }
+
+    if (filters.recibo) {
+        query += ' AND recibo = @recibo';
+    }
+
+    if (filters.abono && filters.abono.min !== null && filters.abono.max !== null) {
+        query += ' AND abono BETWEEN @abonoMin AND @abonoMax';
+    }
+
+    if (filters.availability !== undefined && filters.availability !== null) {
+        query += ' AND availability = @availability';
+    }
+
+    if (filters.sortBy) {
+        const sortOrder = filters.sortOrder === 'asc' ? 'ASC' : 'DESC';
+        query += ` ORDER BY ${filters.sortBy} ${sortOrder}`;
+    }
+    console.log(filters);
+    console.log("query: ", query);
+    try {
+        // Crear una solicitud de consulta
+        const request = pool.request();
+        if (filters.id) {
+            request.input('id', sql.Int, filters.id);
         }
-        try {
-            const query = `
-            INSERT INTO conciliacion_log(id, usr, asesor, asesor_ant, corsario, corsario_ant, recibo, recibo_ant, fecha_validacion, fecha_validacion_ant)
-            VALUES (@id, @usr, @asesor, @asesor_ant, @corsario, @corsario_ant, @recibo, @recibo_ant, @fecha_validacion, @fecha_validacion_ant)
+    
+        if (filters.fecha) {
+        request.input('fecha', sql.Date, filters.fecha);
+        }
+    
+        if (filters.descripcion) {
+        request.input('descripcion', sql.VarChar, `%${filters.descripcion}%`);
+        }
+    
+        if (filters.referencia) {
+        request.input('referencia', sql.VarChar, `%${filters.referencia}%`);
+        }
+    
+        if (filters.referencia_ampliada) {
+        request.input('referencia_ampliada', sql.VarChar, `%${filters.referencia_ampliada}%`);
+        }
+    
+        if (filters.cargo) {
+        request.input('cargo', sql.VarChar, filters.cargo);
+        }
+    
+        if (filters.proveedor) {
+        request.input('proveedor', sql.VarChar, filters.proveedor);
+        }
+    
+        if (filters.asesor) {
+        request.input('asesor', sql.VarChar, filters.asesor);
+        }
+    
+        if (filters.corsario) {
+        request.input('corsario', sql.VarChar, filters.corsario);
+        }
+    
+        if (filters.recibo) {
+        request.input('recibo', sql.VarChar, filters.recibo);
+        }
+    
+        if (filters.abono && filters.abono.min !== null && filters.abono.max !== null) {
+        request.input('abonoMin', sql.Float, filters.abono.min);
+        request.input('abonoMax', sql.Float, filters.abono.max);
+        }
+    
+        if (filters.availability !== undefined && filters.availability !== null) {
+        request.input('availability', sql.Bit, filters.availability);
+        }
+        console.log("params: ", request.params);
+        // Ejecutar la consulta
+        const result = await request.query(query);
+        console.log(result.recordset);
+        console.log(result.recordset.length);
+
+        res.status(200).json(result.recordset);
+
+        /*
+        const result = await pool.request().query('SELECT * FROM conciliacion');
+        console.log(result.recordset);
+        res.status(200).json(result.recordset);
+        */
+    } catch (err) {
+        console.error('Error al obtener datos:', err);
+        res.status(500).send(err.message);
+    }
+});
+
+app.post('/addDataLog/', async (req, res) => {
+    const { id, usr, asesor, asesor_ant, corsario, corsario_ant, recibo, recibo_ant, fecha_validacion, fecha_validacion_ant } = req.body;
+    if (!id && !usr && !asesor && !asesor_ant && !corsario && !corsario_ant && !recibo && !recibo_ant && !fecha_validacion && !fecha_validacion_ant) {
+        return res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar' });
+    }
+    try {
+        const query = `
+        INSERT INTO conciliacion_log(id, usr, asesor, asesor_ant, corsario, corsario_ant, recibo, recibo_ant, fecha_validacion, fecha_validacion_ant)
+        VALUES (@id, @usr, @asesor, @asesor_ant, @corsario, @corsario_ant, @recibo, @recibo_ant, @fecha_validacion, @fecha_validacion_ant)
+        `;
+        await pool.request()
+            .input('id', sql.Int, id)
+            .input('usr', sql.VarChar, usr)
+            .input('asesor', sql.VarChar, asesor)
+            .input('asesor_ant', sql.VarChar, asesor_ant)
+            .input('corsario', sql.VarChar, corsario)
+            .input('corsario_ant', sql.VarChar, corsario_ant)
+            .input('recibo', sql.VarChar, recibo)
+            .input('recibo_ant', sql.VarChar, recibo_ant)
+            .input('fecha_validacion', sql.DateTime, new Date(fecha_validacion))
+            .input('fecha_validacion_ant', sql.DateTime, new Date(fecha_validacion_ant))
+            .query(query);
+        res.status(201).json({ message: 'Registro creado exitosamente' });
+    } catch (err) {
+        console.error('Error al agregar el registro:', err);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+app.post('/uploadData/', async (req, res) => {
+    const data_list = req.body;
+
+    if (!(data_list.length>0)) {
+        console.log(res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar' }));
+        return res.status(400).json({ message: 'Debe enviar al menos un campo para actualizar' });
+    }
+    try {
+        console.log(data_list);
+        for (const item of data_list) {
+            // Verificar si el registro ya existe
+            const existsQuery = `
+              SELECT * FROM conciliacion
+              WHERE fecha = @fechaOperacion
+              AND descripcion = @concepto
+              AND referencia = @referencia
+              AND referencia_ampliada = @referenciaAmpliada
+              AND estatus = @cargo
+              AND abono = @abono
             `;
 
-            const result = await pool.request()
-                .input('id', sql.Int, id)
-                .input('usr', sql.VarChar, usr)
-                .input('asesor', sql.VarChar, asesor)
-                .input('asesor_ant', sql.VarChar, asesor_ant)
-                .input('corsario', sql.VarChar, corsario)
-                .input('corsario_ant', sql.VarChar, corsario_ant)
-                .input('recibo', sql.VarChar, recibo)
-                .input('recibo_ant', sql.VarChar, recibo_ant)
-                .input('fecha_validacion', sql.DateTime, new Date(fecha_validacion))
-                .input('fecha_validacion_ant', sql.DateTime, new Date(fecha_validacion_ant))
-                .query(query);
-            console.log(result.output);
-            console.log(result);
-            res.status(201).json({ message: 'Registro creado exitosamente' });
-        } catch (err) {
-            console.error('Error al agregar el registro:', err);
-            res.status(500).json({ message: 'Error interno del servidor' });
-        }
-    });
+            /*
+                
+                 
+              
+              
+                
+            */
+            console.log(format(item['Fecha Operación'], 'yyyy-MM-dd'));
+            const fecha = new Date(item['Fecha Operación']);
+            // "Hack": agrega la diferencia de tu zona horaria en minutos
+            fecha.setMinutes(fecha.getMinutes() + fecha.getTimezoneOffset());
+
+            //const resultado = format(fecha, 'yyyy-MM-dd');
+            console.log("resultado: ", format(fecha, 'yyyy-MM-dd')); 
+            
+            //console.log(new Date(item['Fecha Operación']));
+            const existsResult = await pool.request()
+              .input('fechaOperacion', sql.DateTime, format(fecha, 'yyyy-MM-dd'))
+              .input('concepto', sql.VarChar, item['Concepto'])
+              .input('referencia', sql.VarChar, item['Referencia'])
+              .input('referenciaAmpliada', sql.VarChar, item['Referencia Ampliada'])
+              .input('cargo',  sql.VarChar,item['Cargo']==null ? 'IS NULL':item['Cargo']=='' ? '' : item['Cargo'])
+              .input('abono', sql.Float, item['Abono'] ? parseFloat(item['Abono']) : null)
+              .query(existsQuery);
+            console.log(existsResult.recordset);
+            console.log('Consulta (parametrizada):', existsQuery);
+            console.log('Parámetros: ', format(fecha, 'yyyy-MM-dd'));
+            console.log('Parámetros: ', item['Concepto']);
+            console.log('Parámetros: ', item['Referencia']);
+            console.log('Parámetros: ', item['Referencia Ampliada']);
+            console.log('Parámetros: ', item['Cargo']==null ? 'IS NULL':item['Cargo']=='' ? '' : item['Cargo']);
+            console.log('Parámetros: ', item['Abono'] ? parseFloat(item['Abono']) : null);
+            
+            console.log(item['Cargo']==null ? 'IS NULL':item['Cargo']=='' ? '' : item['Cargo']);
+            // Si no existe, insertar el registro
+            if (existsResult.recordset.length === 0) {
+              const insertQuery = `
+                INSERT INTO conciliacion (fecha, descripcion, referencia, referencia_ampliada, estatus, abono)
+                VALUES (@fechaOperacion, @concepto, @referencia, @referenciaAmpliada, @cargo, @abono)
+              `;
+            /*
+              await pool.request()
+                .input('fechaOperacion', sql.DateTime, new Date(item['Fecha Operación']))
+                .input('concepto', sql.VarChar, item['Concepto'])
+                .input('referencia', sql.VarChar, item['Referencia'])
+                .input('referenciaAmpliada', sql.VarChar, item['Referencia Ampliada'])
+                .input('cargo', sql.VarChar, item['Cargo'] ? parseFloat(item['Cargo']) : null)
+                .input('abono', sql.Float, item['Abono'] ? parseFloat(item['Abono']) : null)
+                .query(insertQuery);
+            */
+              console.log('Registro insertado:', item);
+            } else {
+              console.log('Registro ya existente:', item);
+            }
+          }
+      
+          res.status(200).json({ message: 'Verificación e inserción completadas' });
+        
+    } catch (err) {
+        console.error('Error al agregar el registro:', err);
+        res.status(500).json({ message: 'Error interno del servidor' });
+    }
+});
+
+// Otros endpoints...
 
     // Endpoint para actualizar datos
     app.put('/updateData/:id', async (req, res) => {
@@ -144,56 +379,5 @@ sql.connect(dbConfig).then(pool => {
     });
     
 
-}).catch(err => console.error('Error de conexión:', err));
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`API corriendo en el puerto ${port}`));
-//app.listen(3000, () => console.log('API corriendo en el puerto 3000'));
-
-
-
-
-
-
-/*
-
-const express = require('express');
-const sql = require('mssql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-// Configuración de conexión a Azure SQL
-const dbConfig = {
-    user: 'admin_viajes_lili',
-    password: '$3<Leca8#LP,5y9',
-    server: 'server-viajes-lili.database.windows.net',
-    database: 'db_viajes_lili',
-    options: {
-        encrypt: true, // Asegura la conexión
-    }
-};
-
-sql.connect(dbConfig).then(pool => {
-    console.log('Conexión exitosa a Azure SQL');
-
-    // Ejemplo: Endpoint para obtener datos
-    app.get('/getData', async (req, res) => {
-        try {
-            const result = await pool.request().query('SELECT * FROM conciliacion');
-            //print(result.recordset)
-            res.status(200).json(result.recordset);
-        } catch (err) {
-            res.status(500).send(err.message);
-        }
-    });
-
-}).catch(err => console.error('Error de conexión:', err));
-
-app.listen(3000, () => console.log('API corriendo en el puerto 3000'));
-
-*/
